24e09976b2770e5696ba075c1604294b505eb5c1
LittleMangoez <matthaydensmith9@gmail.com>
2024-10-06 17:14:27 -0400

    Initial Commit

---
+++ BakedBread/BakedBreadApp.swift
@@ -0,0 +1,17 @@
+//
+//  BakedBreadApp.swift
+//  BakedBread
+//
+//  Created by Matthew Smith on 10/6/24.
+//
+
+import SwiftUI
+
+@main
+struct BakedBreadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BakedBreadTests/BakedBreadTests.swift
@@ -0,0 +1,17 @@
+//
+//  BakedBreadTests.swift
+//  BakedBreadTests
+//
+//  Created by Matthew Smith on 10/6/24.
+//
+
+import Testing
+@testable import BakedBread
+
+struct BakedBreadTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
